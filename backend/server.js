ec618fe7fdcb427b9c28c127b18afe214f0f3dcc
danielchen <danielyilong@outlook.com>
2025-02-08 20:20:55 -0500

    Complete backend for uploading and retrieving images

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const dbo = require('./db/conn'); 
+const dbo = require("./db/conn");
 const cors = require("cors");
 
 // Specify all of the backend routes for the server
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require("./routes/authRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 
 const router = express.Router();
 const app = express();
@@ -16,13 +17,14 @@ app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/images", uploadRoutes);
 
 app.listen(PORT, () => {
-    // perform a database connection when server starts
-    dbo.connectToServer(function (err) {
-      if (err) console.error(err);
-     });
-     console.log(`Server is running on http://localhost:${PORT}`);
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
   });
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = router;
