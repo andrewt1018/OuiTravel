1504b77ae901dee7b82faa0227f52a91b4da3498
andrewtan1018 <tan434@purdue.edu>
2025-02-14 17:18:07 -0500

    small merge conflicts

--- frontend/src/components/Index.js
+++ frontend/src/components/Index.js
@@ -1,5 +1,5 @@
 import {React, useEffect, useState} from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import {getUser} from './helpers/user-verification'
 
 const Index = () => {
