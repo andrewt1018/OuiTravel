184333703e767004c6bc425844d9ae4bc29146b6
Elisa <chen4280@purdue.edu>
2025-02-25 20:00:08 -0500

    Location object + page

--- backend/modules/Location.js
+++ backend/modules/Location.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const locationSchema = new mongoose.Schema({
+  placeId: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
