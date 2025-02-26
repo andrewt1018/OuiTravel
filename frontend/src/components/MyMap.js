d1d28448f6883fbd39945855187b30bb743e193e
andrewtan1018 <tan434@purdue.edu>
2025-02-25 19:33:58 -0500

    Removed extra space in save icon

--- frontend/src/components/MyMap.js
+++ frontend/src/components/MyMap.js
@@ -298,11 +298,6 @@ function MyMap() {
                             }}
                         >
                                 <div>
-                                    <p>
-                                        Save icon!
-                                    </p>
-                                    <br />
-                                    <br /><br /><br />
                                     <button
                                         type="submit"
                                         className={`w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm`}
